6409933cf25982ede3692e62cbbb99e7a90243ea
1970186643 <1970186643@qq.com>
2020-04-03 14:32:41 +0800

    实现登入组件

---
+++ vue-shop/src/components/Login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!-- 表单 -->
+      <el-form  label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary">登入</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.login_box .avatar_box{
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+.login_box .avatar_box img{
+  width: 100%;
+  width: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns{
+  display: flex;
+  justify-content: center;
+}
+</style>
