0fd7193daf398655b3b46036cbe87465a9084c00
1970186643 <1970186643@qq.com>
2020-04-05 23:21:29 +0800

    modify some message

--- vue-shop/README.md
+++ vue-shop/README.md
@@ -1,2 +1,8 @@
 - lang="less"
 npm install less less-loader --sava-dev
+
+- git merge login
+合并login分支，可以把以前分支的东西进行合并
+
+- git push -u origin login
+第一次推送login分支
--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <!-- 头部主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -19,5 +27,16 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.home-container{
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside{
+  background-color: #333744;
+}
+.el-main{
+  background-color: #EAEDF1;
+}
 </style>
